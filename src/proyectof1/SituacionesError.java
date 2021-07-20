2b99f6d09eeda3a9e8d39c09fb79fc7fa2d89275
Daniel250Gue <84732717+Daniel250Gue@users.noreply.github.com>
2021-07-20 16:26:35 -0500

    Registro inventario listo

--- src/proyectof1/SituacionesError.java
+++ src/proyectof1/SituacionesError.java
@@ -6,28 +6,40 @@ import javax.swing.JOptionPane;
 
 public class SituacionesError {
     
-    public void ErrorDatos(String nombre, String cantidad, String id)
+    public void ErrorDatos(String nombre, String cantidad, String id, String precio)
     {
-        if (cantidad.equals("")&& id.equals("")&&!nombre.equals(""))
-         JOptionPane.showMessageDialog(null,"Ingrese la cantidad y La ID del producto");
+        if (cantidad.equals("")&& id.equals("")&& precio.equals("")&&!nombre.equals(""))
+         JOptionPane.showMessageDialog(null,"Ingrese la cantidad , La ID y precio del producto");
                              
-        else if (id.equals("") && nombre.equals("")&&!cantidad.equals(""))
-        JOptionPane.showMessageDialog(null,"Ingrese el ID del producto y Nombre");
+        else if (id.equals("") && nombre.equals("")&& precio.equals("")&&!cantidad.equals(""))
+        JOptionPane.showMessageDialog(null,"Ingrese el ID , Nombre y precio del producto");
                              
-        else if (nombre.equals("") && cantidad.equals("")&&!id.equals(""))
-        JOptionPane.showMessageDialog(null,"Ingresar nombre del articulo y Cantidad de activos");
+        else if (nombre.equals("") && cantidad.equals("")&& precio.equals("")&&!id.equals(""))
+        JOptionPane.showMessageDialog(null,"Ingresar Nombre , Cantidad y Precio del producto");
         
-        else if (nombre.equals(""))
-            JOptionPane.showMessageDialog(null,"Ingrese el nombre del articulo");
+        else if (nombre.equals("") && cantidad.equals("")&& precio.equals("")&&!precio.equals(""))
+        JOptionPane.showMessageDialog(null,"Ingresar Nombre , Cantidad y ID del producto");
+        
+        
+        else if (!nombre.equals("") && !cantidad.equals("")&& !id.equals("")&&precio.equals(""))
+            JOptionPane.showMessageDialog(null,"Ingrese precio");
+            
+           else if (nombre.equals("") && cantidad.equals("")&& id.equals("")&& precio.equals(""))   
+               JOptionPane.showMessageDialog(null,"Debe llenar todos los campos");
+           
+           
+          else if (nombre.equals(""))
+            JOptionPane.showMessageDialog(null,"Debe Introducir un nombre");
         
         else if (cantidad.equals(""))
             JOptionPane.showMessageDialog(null,"Ingrese la cantidad del articulo");
         
         else if (id.equals(""))
             JOptionPane.showMessageDialog(null,"Ingrese el ID del producto");
+        
+        else if (id.equals(""))
+        JOptionPane.showMessageDialog(null,"Debe Introducir el precio");
             
-        else 
-         JOptionPane.showMessageDialog(null,"Debe llenar todos los campos");
         
         
     }
